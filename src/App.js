9b064e63fe79dbbdba62caf6ceafea36ca4904f9
Mehmet Konukcu <mehmetext68@gmail.com>
2022-07-14 02:18:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactTextareaAutosize from "react-textarea-autosize";
+import "./css/style.css";
+import { hashtagMaps, readySettingMaps, extraDescriptionText } from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [form, setForm] = useState({
+		extraDescription: extraDescriptionText,
+		hashtags: "",
+		description: "",
+	});
+	const [showingPopup, setShowingPopup] = useState(false);
+
+	const onChangeInput = (e) => {
+		setForm({ ...form, [e.target.id]: e.target.value });
+	};
+
+	const onClickReadySetting = (e) => {
+		setForm({
+			...form,
+			description: e.description,
+			hashtags: generateHashtagStr(e.tags),
+			extraDescription: extraDescriptionText,
+		});
+	};
+
+	const onClickHashtagGroup = (e) => {
+		setForm({
+			...form,
+			hashtags: generateHashtagStr(e.tags),
+		});
+	};
+
+	const generateHashtagStr = (oldTags) => {
+		let tags = [...oldTags];
+		let mixed = [];
+
+		tags.forEach((e) => {
+			let index = Math.floor(Math.random() * tags.length);
+			mixed = [...mixed, tags[index]];
+			tags = tags.filter((tag) => tag !== tags[index]);
+		});
+
+		let tagsStr = "#" + mixed[0];
+
+		for (let i = 0; i < mixed.length; i++) {
+			tagsStr += ", #" + mixed[i];
+		}
+
+		return tagsStr;
+	};
+
+	const copyClipboard = async (e) => {
+		console.log("kopyalandı");
+		let copying = form.description + form.extraDescription + form.hashtags;
+		await navigator.clipboard.writeText(copying);
+
+		setShowingPopup(true);
+		setTimeout(() => {
+			setShowingPopup(false);
+		}, 1000);
+	};
+
+	return (
+		<>
+			<div className="readySettings">
+				<div className="container">
+					<div className="readySettings_title">HAZIR AYARLAR</div>
+					<div className="readySettings_content">
+						{readySettingMaps.map((element) => (
+							<div
+								key={element.id}
+								className="readySettingItem"
+								onClick={(e) => onClickReadySetting(element)}
+							>
+								{element.name}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+			<div className="input-group">
+				<div className="container">
+					<h2>Açıklama</h2>
+					<ReactTextareaAutosize
+						name="description"
+						id="description"
+						className="input_textarea"
+						minRows={2}
+						value={form.description}
+						onChange={onChangeInput}
+					/>
+					<div onClick={copyClipboard} className="copy-small">
+						KOPYALA
+					</div>
+				</div>
+			</div>
+			<div className="input-group">
+				<div className="container">
+					<h2>Etiketler</h2>
+					<div className="hashtags">
+						{hashtagMaps.map((element) => (
+							<div
+								key={element.id}
+								className="hashtagItem"
+								onClick={() => onClickHashtagGroup(element)}
+							>
+								{element.name}
+							</div>
+						))}
+					</div>
+					<ReactTextareaAutosize
+						name="hashtags"
+						id="hashtags"
+						className="input_textarea"
+						minRows={2}
+						onChange={onChangeInput}
+						value={form.hashtags}
+					/>
+				</div>
+			</div>
+			<div className="input-group">
+				<div className="container">
+					<h2>Ek Açıklama</h2>
+					<ReactTextareaAutosize
+						name="extraDescription"
+						id="extraDescription"
+						className="input_textarea"
+						minRows={2}
+						onChange={onChangeInput}
+						value={form.extraDescription}
+					/>
+				</div>
+			</div>
+			<div className="copy" onClick={copyClipboard}>
+				KOPYALA
+			</div>
+			<div className={"copied-popup " + (showingPopup ? "show" : "")}>
+				<h3>KOPYALANDI</h3>
+			</div>
+			<div style={{ height: "8rem" }}></div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,175 @@
+export const extraDescriptionText = `
+
+•  •  •  •  •  •  •
+
+Daha fazla bu tarz içerik için gönderiyi beğenebilir, içerik hakkında fikrinizi belirtmek için yorum yapabilir, daha sonra işine yarayacağı için gönderiyi kaydedebilir ve gönderilerden haberdar olmak için sayfayı takip edebilirsin 🧡
+
+📢 Ayrıca paylaşımlarımdan ilk sen haberdar olmak istiyorsan gönderi bildirimlerini açmayı unutma. Sayfayı takip ettikten sonra sağ üst köşede bulunan zil butonuna basman yeterlidir.
+
+•  •  •  •  •  •  •
+
+📌 Algoritmalar ve çözümleri
+📌 Dart (Programlama Dili) dersleri
+📌 Flutter (Mobil Programlama) dersleri, örnekleri ve kodları
+📌 Programlama dillerindeki fonksiyonların mantığı
+📌 Yazılımdaki ilerlemem ve gelişimim
+📌 Sizlerden gelen sorular hakkında tartışmalar ve görseller
+📌 Motivasyon ve başarı hikayeleri
+⭐ Ve daha fazlası için sayfayı takip etmeyi unutmayın!
+
+•  •  •  •  •  •  •
+
+`;
+
+export const hashtagMaps = [
+	{
+		id: "general",
+		name: "Genel",
+		tags: [
+			"bilgisayar",
+			"bikodist",
+			"flutter",
+			"dartlang",
+			"yazılım",
+			"yazılımcı",
+			"kodlama",
+			"kod",
+			"mühendis",
+			"yazılımmühendisliği",
+			"bilgisayarmühendisliği",
+			"coder",
+			"developer",
+			"mobilprogramlama",
+			"geliştirici",
+		],
+	},
+	{
+		id: "javascript",
+		name: "Javascript",
+		tags: [
+			"bilgisayar",
+			"bikodist",
+			"yazılım",
+			"yazılımcı",
+			"webgelistirici",
+			"webgeliştirici",
+			"frontend",
+			"javascript",
+			"kodlama",
+			"kod",
+			"mühendis",
+			"yazılımmühendisliği",
+			"bilgisayarmühendisliği",
+			"developer",
+			"mobilprogramlama",
+			"geliştirici",
+		],
+	},
+	{
+		id: "dart_flutter",
+		name: "Dart - Flutter",
+		tags: [
+			"mobil",
+			"bikodist",
+			"dartlang",
+			"flutter",
+			"android",
+			"ios",
+			"crossplatform",
+			"yazılımcı",
+			"developer",
+			"kodlama",
+			"programlama",
+			"mobilprogramlama",
+			"mobileprogramming",
+			"gelistirici",
+			"geliştirici",
+		],
+	},
+	{
+		id: "algorythm",
+		name: "Algoritma",
+		tags: [
+			"bikodist",
+			"algoritma",
+			"algorythm",
+			"yazılımcı",
+			"yazilimci",
+			"yazılım",
+			"programlama",
+			"kodlama",
+			"kod",
+			"yazılımmühendisliği",
+			"bilgisayarmühendisliği",
+			"developer",
+			"geliştirici",
+		],
+	},
+	{
+		id: "motivation",
+		name: "Motivasyon",
+		tags: [
+			"bilgisayar",
+			"bikodist",
+			"yazilim",
+			"coder",
+			"yazılım",
+			"developer",
+			"gelistirici",
+			"geliştirici",
+			"motivasyon",
+			"programlama",
+			"mobilprogramlama",
+			"basari",
+			"başarı",
+		],
+	},
+];
+
+export const readySettingMaps = [
+	{
+		id: "dart_question",
+		name: "Dart Sorusu",
+		description:
+			"Bilme sırası sende. Hadi bil bakalım, doğru cevap hangisi? ⭐",
+		tags: hashtagMaps[2].tags,
+	},
+	{
+		id: "javascript_day",
+		name: "Javascript Gün",
+		description: "Javascript'te 6. günüm 🤩🥳 Bugün",
+		tags: hashtagMaps[1].tags,
+	},
+	{
+		id: "motivation",
+		name: "Motivasyon",
+		description: "",
+		tags: hashtagMaps[4].tags,
+	},
+	{
+		id: "word_puzzle",
+		name: "Kelime Bulmaca",
+		description:
+			'Yazılım terimleriyle oluşturulmuş kelime bulmacada 5 kelime bulunuyor. Kelimeleri yorumlara "," ile ayırarak yazabilirsiniz.',
+		tags: hashtagMaps[0].tags,
+	},
+	{
+		id: "dart_flutter_lesson",
+		name: "Dart Flutter Ders",
+		description: "",
+		tags: hashtagMaps[2].tags,
+	},
+	{
+		id: "algorythm",
+		name: "Algoritma",
+		description:
+			"Algoritma sorusunun cevabını bulabilecek misin? Kendine güveniyorsan seni yorumlara alalım!",
+		tags: hashtagMaps[3].tags,
+	},
+	{
+		id: "general",
+		name: "Genel",
+		description: "",
+		tags: hashtagMaps[0].tags,
+	},
+];
